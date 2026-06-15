05c18632e4db16ea147f4031cae53da6cd739b30
Krishanu Mondal <mkrishanu332@gmail.com>
2026-06-15 23:03:22 +0530

    style: format cron scheduler

--- backend/src/jobs/monitor.job.js
+++ backend/src/jobs/monitor.job.js
@@ -1,11 +1,15 @@
 import cron from "node-cron";
+
 import { runChecks }
 from "../workers/monitor.worker.js";
 
-cron.schedule("* * * * *", async () => {
-  console.log(
-    "Running monitor checks..."
-  );
+cron.schedule(
+  "* * * * *",
+  async () => {
+    console.log(
+      "Running monitor checks..."
+    );
 
-  await runChecks();
-});
+    await runChecks();
+  }
+);
